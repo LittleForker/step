de947d9c20091de97e94213daffb1956d97b71ae
Alan Dipert <alan@dipert.org>
2010-07-25 23:04:30 -0400

    alandipert/step has moved to scalatra/scalatra

--- core/src/main/scala/org/scalatra/Initializable.scala
+++
@@ -1,9 +0,0 @@
-package org.scalatra
-
-/**
- * Unifies the initialization process of a Servlet and a Filter.  This is
- * useful for mixins that depend on the ServletConfig/FilterConfig.
- */
-trait Initializable {
-  def init(config: Config)
-}
--- core/src/test/scala/org/scalatra/ScalatraSuite.scala
+++
@@ -1,9 +0,0 @@
-package org.scalatra
-
-import org.scalatest.FunSuite
-import test.ScalatraTests
-import org.scalatest.junit.JUnitRunner
-import org.junit.runner.RunWith
-
-@RunWith(classOf[JUnitRunner])
-class ScalatraSuite extends FunSuite with ScalatraTests
